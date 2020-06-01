700b5f70980590db53d50594d84ddb9705d8f3ca
nidhu23 <nidhunanik@gmail.com>
2020-06-02 00:36:44 +0530

    Added: TotalWorking hours or days

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 echo "Welcome to employee wage computation "
-partTime=1
-fullTime=2
+partTime=1;
+fullTime=2;
 ratePerHr=20;
+maxHrs=100
 workingDays=20;
 totalSalary=0;
-for (( i=1; i<=$workingDays; i++ ))
+totalEmpHrs=0;
+totalWorkingDays=0;
+while [[ $totalEmpHrs -lt $maxHrs && $totalWorkingDays -lt $workingDays ]]
 do
+        ((totalWorkingDays++))
         empCheck=$(($RANDOM%3))
         case $empCheck in
         	$fullTime) empHrs=8;;
         	$partTime) empHrs=4;;
                    	*) empHrs=0;;
 esac
-salary=$((empHrs*ratePerHr))
-totalSalary=$(($salary+$totalSalary))
+totalEmpHrs=$(($totalEmpHrs+$empHrs))
 done
+totalSalary=$(($totalEmpHrs*$ratePerHr))
 echo $totalSalary
