0aedd81e1ce760436976b058b5c3adfb6c34054d
nidhu23 <nidhunanik@gmail.com>
2020-06-02 00:42:40 +0530

    Refactored: by adding a fuction to compute wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -8,16 +8,21 @@ workingDays=20;
 totalSalary=0;
 totalEmpHrs=0;
 totalWorkingDays=0;
+
+function  getWorkingHours(){
+case $1 in
+        $fullTime) empHrs=8;;
+        $partTime) empHrs=4;;
+                   *) empHrs=0;;
+esac
+echo $empHrs
+}
 while [[ $totalEmpHrs -lt $maxHrs && $totalWorkingDays -lt $workingDays ]]
 do
         ((totalWorkingDays++))
-        empCheck=$(($RANDOM%3))
-        case $empCheck in
-        	$fullTime) empHrs=8;;
-        	$partTime) empHrs=4;;
-                   	*) empHrs=0;;
-esac
-totalEmpHrs=$(($totalEmpHrs+$empHrs))
+        #empCheck=$(($RANDOM%3))
+        WorkingHrs=$(getWorkingHours  $(($RANDOM%3)) )
+        totalEmpHrs=$(($totalEmpHrs+$WorkingHrs))
 done
 totalSalary=$(($totalEmpHrs*$ratePerHr))
 echo $totalSalary
